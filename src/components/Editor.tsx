8ca6400e1e922875be01014b4b888e053ffd250a
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-03-04 22:03:10 +0530

    Add editor design

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useCallback, useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 const Editor = () => {
-  return <div>Editor</div>;
+  const [value, setValue] = useState("console.log('hello world!');");
+  const onChange = useCallback((val, viewUpdate) => {
+    setValue(val);
+  }, []);
+  return (
+    <div className="editor">
+      <CodeMirror
+        value={value}
+        // height="200px"
+        theme={"dark"}
+        // theme={vscodeDark}
+        minHeight="90vh"
+        extensions={[
+          markdown({ base: markdownLanguage, codeLanguages: languages }),
+        ]}
+        onChange={onChange}
+        autoCorrect="true"
+        autoSave="true"
+      />
+      ;
+    </div>
+  );
 };
 
 export default Editor;
